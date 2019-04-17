238464cc03f845b56351c3d58f5539aa1a392879
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-04-17 17:44:06 -0400

    Create python-setup.bat

---
+++ Windows/python-setup.bat
@@ -0,0 +1,8 @@
+::install commonly used python extensions
+
+pip install pyperclip
+pip install python-docx
+pip install DateTime
+pip install usaddress
+pip install requests
+pip install beautifulsoup4
