226b6dc0960d7c1091f986b9fd6ddfd1db77e0e3
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-04-17 17:52:47 -0400

    Create install.sh

---
+++ Linux/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#Installation script for Linux Mint Rosa (19)
+
+#install sublimetext
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+#install brave
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com `bionic` main" >> /etc/apt/sources.list.d/brave.list'
+sudo apt install brave-browser brave-keyring
