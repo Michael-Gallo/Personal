787ebe626acb4a135d7c5867443b67ac66d1d6e0
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-04-27 19:25:29 -0400

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -27,3 +27,6 @@ sudo dpkg -i discord.deb
 #install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#install skype for linux
+sudo apt-get install skypeforlinux
