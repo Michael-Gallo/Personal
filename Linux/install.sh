90516d0ccf49e6402c66d5c37060bc52a387014d
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-04-27 01:31:09 -0400

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -3,6 +3,9 @@
 
 #Installation script for Linux Mint Rosa (19)
 
+#install all dependencies
+sudo apt-get install -f
+
 #install sublimetext
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
@@ -10,7 +13,17 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get install sublime-text
 
+sudo apt-get install -f
 #install brave
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com `bionic` main" >> /etc/apt/sources.list.d/brave.list'
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com bionic main" >> /etc/apt/sources.list.d/brave.list'
+sudo apt-get install -f
 sudo apt install brave-browser brave-keyring
+
+#install discord
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+
+#install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
