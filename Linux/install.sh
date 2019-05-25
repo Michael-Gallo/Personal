5f1117477042a00cf1f4b2481abfe90ac9b59204
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-05-25 14:19:45 -0400

    replaced repetitive lines with a forloop

--- Linux/install.sh
+++ Linux/install.sh
@@ -3,22 +3,14 @@
 
 #Installation script for Linux Mint Rosa (19)
 
-#install all dependencies
-sudo apt-get install -f
 
 #install sublimetext
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
-
-sudo apt-get install -f
 #install brave
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com bionic main" >> /etc/apt/sources.list.d/brave.list'
-sudo apt-get install -f
-sudo apt install brave-browser brave-keyring
 
 #install discord
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
@@ -28,13 +20,12 @@ sudo dpkg -i discord.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-#install skype for linux
-sudo apt-get install skypeforlinux -y
 
-#install XdoTool (needed for keyboard shortcuts that type out emails)
-sudo apt-get install xdotool -y
+#Install packages now that all repos are set
+apps = "sublime-text brave-browser brave-keyring skypeforlinux xdotool steam-installer imwheel"
 
-#install steam 
-sudo apt-get install steam-installer -y
-#instal imwheel
-sudo apt-get install imwheel -y
+for app in $apps
+do
+	sudo apt install $app -y
+	sudo apt install -f
+done
