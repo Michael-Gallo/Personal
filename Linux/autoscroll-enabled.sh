b6e32dd6da074b402a61092c873879d7419a8b70
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-07-24 20:06:50 -0400

    Update autoscroll-enabled.sh
    Made true name matching for G600, removed unnecessary lines

--- Linux/autoscroll-enabled.sh
+++ Linux/autoscroll-enabled.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-xinput --set-prop pointer:"Logitech Gaming Mouse G600" 'Evdev Wheel Emulation' 1
-xinput --set-prop pointer:"Logitech Gaming Mouse G600" 'Evdev Wheel Emulation Button' "2"
-xinput --set-prop pointer:"Logitech Gaming Mouse G600" 'Evdev Wheel Emulation Axes' 6 7 4 5
-xinput --set-prop pointer:"Logitech Gaming Mouse G600" "libinput Scroll Method Enabled" 0 0 1
-xinput --set-prop pointer:"Logitech Gaming Mouse G600" "libinput Scroll Method Enabled" 0 0 1
+for id in `xinput --list|grep 'Logitech Gaming Mouse G600'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
+	xinput --set-prop $id 'Evdev Wheel Emulation' 1
+	xinput --set-prop $id 'Evdev Wheel Emulation Button' "2"
+	xinput --set-prop $id 'Evdev Wheel Emulation Axes' 6 7 4 5
+done
