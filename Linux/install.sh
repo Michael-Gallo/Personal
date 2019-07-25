56b36310723c8cd2b0f7c901811432b11a3f932d
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-07-24 20:09:10 -0400

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -4,21 +4,10 @@
 #Installation script for Linux Mint Rosa (19)
 
 
-#install sublimetext
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
 #install brave
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com bionic main" >> /etc/apt/sources.list.d/brave.list'
 
-#install discord
-wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-sudo dpkg -i discord.deb
-
-#install chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 
 #Install packages now that all repos are set
