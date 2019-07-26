5a55bebc6550fece4397a93d960021d0fd83a2bc
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-07-25 21:26:45 -0400

    Update autoscroll-enabled.sh

--- Linux/autoscroll-enabled.sh
+++ Linux/autoscroll-enabled.sh
@@ -4,4 +4,6 @@ for id in `xinput --list|grep 'Logitech Gaming Mouse G600'|perl -ne 'while (m/id
 	xinput --set-prop $id 'Evdev Wheel Emulation' 1
 	xinput --set-prop $id 'Evdev Wheel Emulation Button' "2"
 	xinput --set-prop $id 'Evdev Wheel Emulation Axes' 6 7 4 5
+	xinput --set-prop $id "libinput Scroll Method Enabled" 0 0 1
+	xinput --set-prop $id "libinput Scroll Method Enabled" 0 0 1
 done
