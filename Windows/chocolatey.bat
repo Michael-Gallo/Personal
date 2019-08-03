6d92b3f09e95ce7094bb2d95b6c49562228eabd9
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-08-02 23:24:58 -0400

    turned the chocolatey program installation into a forloop

--- Windows/chocolatey.bat
+++ Windows/chocolatey.bat
@@ -1,22 +1,8 @@
 :: Install Chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-:: Install Software with Cocolatey 
-choco install foxitreader -y
-choco install brave -y
-choco install googlechrome -y
-choco install sublimetext3 -y
-choco install playnite -y
-choco install discord -y
-choco install jre8 -y
-choco install 7zip -y
-choco install autohotkey -y
-choco install ccleaner -y
-choco install speccy -y
-choco install python -y
-choco install paint.net -y
-choco install thunderbird -y
-choco install itunes -y
-choco install procexp -y
-choco install k-litecodecpackfull -y
-choco install calibre -y
+set list=foxitreader brave chromium sublimetext3 discord playnite jre8 7zip autohotkey ccleaner speccy python paint.net itunes
+:: Install Software with Chocolatey 
+(FOR %%p IN (%list%) DO (
+  choco install %%p -y
+  ))
