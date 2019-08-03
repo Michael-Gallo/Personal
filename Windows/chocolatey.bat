91a7661ce846167d816a557604c03232b8c39277
Michael Gallo <44709959+Michael-Gallo@users.noreply.github.com>
2019-08-02 23:35:17 -0400

    Update chocolatey.bat

--- Windows/chocolatey.bat
+++ Windows/chocolatey.bat
@@ -1,7 +1,7 @@
 :: Install Chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-set list=foxitreader brave chromium sublimetext3 discord playnite jre8 7zip autohotkey ccleaner speccy python paint.net itunes
+set list=foxitreader brave steam chromium sublimetext3 discord playnite jre8 7zip autohotkey ccleaner speccy python paint.net itunes
 :: Install Software with Chocolatey 
 (FOR %%p IN (%list%) DO (
   choco install %%p -y
